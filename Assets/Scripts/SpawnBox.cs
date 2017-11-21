bc7b37b23baa962fd4453629389b6d0d52184f76
sreveloc <sreveloc@gmail.com>
2017-11-21 11:22:35 +0100

    Demo complete

---
+++ Assets/Scripts/SpawnBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBox : MonoBehaviour {
+
+	public GameObject[] boxList;
+	public float CurentVelocity = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+		SpawnNewBox ();
+	}
+	
+	// Update is called once per frame
+	public void SpawnNewBox() {
+		int i = Random.Range (0, boxList.Length);
+		Instantiate (boxList [i], transform.position,  Quaternion.identity);
+	}
+}
